88d5728e263368ea43d1e9b0c8ff269fde8fd245
Bohdan Klymov <programmer.b.k.339@gmail.com>
2026-05-21 12:52:11 +0200

    finished 4 exercises to practice operator skills

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
 public class Main {
-    static void main() {
-        System.out.println("first commit");
+    public static void main() {
+
+//        Exercise 1: Try to find out what the ! operator does by assigning different values to b.
+
+        boolean b = true;
+
+//        !b changes true to false.
+//        If b is already false, !b changes it to true.
+        System.out.println(!b);
+
+//        Exercise 2: Try to find out what the || operator does by assigning different values to a and b.
+
+        boolean x = true;
+        boolean y = false;
+
+//        The result is true if at least one variable is true.
+//        The result is false only if both variables are false.
+        System.out.println(y || x);
+
+
+//        Exercise 3: Try to find out what the || operator does by assigning different values to a and b.
+
+        boolean a = true;
+        boolean c = false;
+
+//        The result is false if at least one variable is false.
+//        The result is true only if both variables are true.
+        System.out.println(a && c);
+
+
+//        Exercise 4:
+
+        int r = 16;
+        int j = 8;
+
+        boolean greaterThan10 = r > 10 && j > 10;
+        System.out.println("r and j are greater than 10: " + greaterThan10);    // false
+
+        boolean smallerThan10 = r < 10 || j < 10;
+        System.out.println("r or j are smaller than 10: " + smallerThan10);     // true
+
+        boolean isTwoTimesBigger = r >= j * 2;
+        System.out.println("r is at least two times bigger than j: " + isTwoTimesBigger);   // true
     }
 }
